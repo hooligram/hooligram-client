1eb49029222bdc70f6d9fed7a876222faff36d99
Imran Ariffin <ariffin.imran@gmail.com>
2019-01-28 23:14:23 -0500

    demo: navigate from `OnboardingAgree` to `OnboardingRequestCode` screen

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
 import { Provider } from 'react-redux'
 import store from '@state/store'
-import { OnboardingRequestCode } from '@screens'
+import OnboardingStackNavigation from '@navigation/onboarding-stack'
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <OnboardingRequestCode/>
+        <OnboardingStackNavigation/>
       </Provider>
     )
   }
