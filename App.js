a666958d19e6273204c40a9a2012f32c91b0050b
Imran Ariffin <ariffin.imran@gmail.com>
2019-01-28 22:30:17 -0500

    inject`hooligram-api` into redux store

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import { Text } from 'react-native'
 import { Provider } from 'react-redux'
 import store from '@state/store'
+import { OnboardingRequestCode } from '@screens'
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Text>Welcome to Hooligram</Text>
+        <OnboardingRequestCode/>
       </Provider>
     )
   }
