56233049338a37dac7332bba52dfbd04c0a8d822
Imran Ariffin <ariffin.imran@gmail.com>
2019-02-18 17:47:30 -0500

    new colors

--- app/constants/Colors.js
+++ app/constants/Colors.js
@@ -1,9 +1,11 @@
 export default {
   black: 'black',
-  boldGreen: '#128c7e',
-  bolderGreen: '#0e776b',
+  lightBlue: '#34B7F1',
+  boldGreen: '#128C7E',
+  bolderGreen: '#0E776B',
   grey: 'grey',
   lightGreen: '#25D366',
+  lighterGreen: '#DCF8C6',
   textLink: '#3366BB',
   white: 'white'
 }
