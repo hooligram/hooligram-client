2a4cbb06cdf4cffb82ed899b6bc27348287f5777
Imran Ariffin <ariffin.imran@gmail.com>
2019-02-17 22:59:16 -0500

    `Conversation` screen header

--- app/constants/Colors.js
+++ app/constants/Colors.js
@@ -1,6 +1,7 @@
 export default {
   black: 'black',
   boldGreen: '#128c7e',
+  bolderGreen: '#0e776b',
   grey: 'grey',
   lightGreen: '#25D366',
   textLink: '#3366BB',
--- app/navigation/conversation-stack.js
+++ app/navigation/conversation-stack.js
@@ -1,9 +1,24 @@
 import { createStackNavigator } from 'react-navigation'
 import { Conversation } from '@screens'
+import { Colors } from '@constants'
+import { StatusBar } from 'react-native'
 
 const routeConfigs = {
   Conversation: {
-    screen: Conversation
+    screen: Conversation,
+    navigationOptions: () => {
+      StatusBar.setBackgroundColor(Colors.bolderGreen)
+
+      return {
+        title: 'Conversation',
+        headerStyle: {
+          backgroundColor: Colors.boldGreen
+        },
+        headerTitleStyle: {
+          color: Colors.white
+        }
+      }
+    }
   }
 }
 
