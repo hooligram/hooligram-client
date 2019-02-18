b06ec8663e9f04ff723d9dcd703017a5affd197e
Imran Ariffin <ariffin.imran@gmail.com>
2019-02-18 14:14:22 -0500

    selectors for `currentUser`s `country_code` & `phone_number`

--- app/state/selectors/__tests__/index.test.js
+++ app/state/selectors/__tests__/index.test.js
@@ -1,10 +1,15 @@
-import { isAuthorized } from '@state/selectors'
+import selectors from '@state/selectors'
 import {
   initialState as authorizationInitialState
 } from '@state/reducers/authorization'
+import {
+  initialState as profileCodeRequestInitialState
+} from '@state/reducers/profile/code-request'
 
 describe('selectors', () => {
   describe('isAuthorized', () => {
+    const { isAuthorized } = selectors
+
     it('should return `true` when all token items available', () => {
       const state = {
         authorization: {
@@ -52,6 +57,60 @@ describe('selectors', () => {
   
         expect(result).toBe(false)
       })
+    })
+  })
+
+  describe('currentUserCountryCode', () => {
+    const { currentUserCountryCode } = selectors
+    let state
+    beforeEach(() => {
+      state = {
+        profile: {
+          codeRequest: { ...profileCodeRequestInitialState }
+        }
+      }
+    })
+
+    it('should return `undefined` when not available', () => {
+      const result = currentUserCountryCode(state)
+
+      expect(result).toEqual(undefined)
+    })
+
+    it('should seelct correct country_code from state', () => {
+      const expected = 'some country code'
+      state.profile.codeRequest.country_code = expected
+
+      const result = currentUserCountryCode(state)
+
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('currentUserPhoneNumber', () => {
+    const { currentUserPhoneNumber } = selectors
+    let state
+    beforeEach(() => {
+      state = {
+        profile: {
+          codeRequest: { ...profileCodeRequestInitialState }
+        }
+      }
+    })
+
+    it('should return `undefined` when not available', () => {
+      const result = currentUserPhoneNumber(state)
+
+      expect(result).toEqual(undefined)
+    })
+
+    it('should seelct correct country_code from state', () => {
+      const expected = 'some phone number'
+      state.profile.codeRequest.phone_number = expected
+
+      const result = currentUserPhoneNumber(state)
+
+      expect(result).toEqual(expected)
     })
   })
 })
--- app/state/selectors/index.js
+++ app/state/selectors/index.js
@@ -4,5 +4,15 @@ export default {
     !!state.authorization.token.code &&
     !!state.authorization.token.country_code &&
     !!state.authorization.token.phone_number
+  ),
+  currentUserCountryCode: (state) => (
+    state.profile.codeRequest.country_code !== ''
+      ? state.profile.codeRequest.country_code
+      : undefined
+  ),
+  currentUserPhoneNumber: (state) => (
+    state.profile.codeRequest.phone_number !== ''
+      ? state.profile.codeRequest.phone_number
+      : undefined
   )
 }
