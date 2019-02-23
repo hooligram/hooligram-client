8d1465b4d944e113f22657234b0e1d23a4f867b6
cglotr <amir.ariffin.920404@gmail.com>
2019-02-23 14:26:00 +0800

    wrap ActivityIndicator as a custom component

--- app/components/index.js
+++ app/components/index.js
@@ -1,2 +1,3 @@
+export { default as ActivityIndicator } from '@hooligram/components/ActivityIndicator'
 export { default as MessageWriter } from '@hooligram/components/MessageWriter'
 export { default as Message } from '@hooligram/components/Message'
