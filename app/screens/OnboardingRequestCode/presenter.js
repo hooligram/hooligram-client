5098d816bfd9c09360fe043ba422ba2f4122b76f
cglotr <amir.ariffin.920404@gmail.com>
2019-02-27 00:03:51 +0800

    add back presenter

---
+++ app/screens/OnboardingRequestCode/presenter.js
@@ -0,0 +1,18 @@
+import { connect as _connect } from 'react-redux'
+import { requestVerificationCode } from '@state/actions/authorization'
+
+export const mapStateToProps = (state) => ({})
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onPressNext: (countryCode, phoneNumber, keyboard) => () => {
+      dispatch(requestVerificationCode(countryCode, phoneNumber))
+      keyboard.dismiss()
+    }
+  }
+}
+
+export const connect = _connect(
+  mapStateToProps,
+  mapDispatchToProps
+)
