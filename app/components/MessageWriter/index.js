f30339b927c39e9c9ff8e5d996ce9b126a276778
cglotr <amir.ariffin.920404@gmail.com>
2019-03-02 10:26:22 +0800

    remove components- let's extract component out on demand basis

--- app/components/MessageWriter/index.js
+++
@@ -1,4 +0,0 @@
-import MessageWriter from '@hooligram/components/MessageWriter/MessageWriter'
-import { connect } from '@hooligram/components/MessageWriter/presenter'
-
-export default connect(MessageWriter)
