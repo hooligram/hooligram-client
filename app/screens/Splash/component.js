16f5fcd212ea010fbe3130fe26fd5045bceb7353
cglotr <amir.ariffin.920404@gmail.com>
2019-04-03 16:26:27 +0800

    polish splash

---
+++ app/screens/Splash/component.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import { ActivityIndicator } from 'hg/components'
+
+export default class Splash extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center'
+        }}
+      >
+        <ActivityIndicator/>
+      </View>
+    )
+  }
+}
