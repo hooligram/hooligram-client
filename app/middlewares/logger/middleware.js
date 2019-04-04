22a6177e395f6410a68c7e252c29b9fed62283c6
cglotr <amir.ariffin.920404@gmail.com>
2019-04-04 17:30:31 +0800

    move to middleware file

---
+++ app/middlewares/logger/middleware.js
@@ -0,0 +1,20 @@
+let actionCount = 0
+const actionTypeCount = new Map()
+
+export default store => next => action => {
+  if (!__DEV__) {
+    return next(action)
+  }
+
+  const actionType = action.type
+  actionCount += 1
+
+  if (!actionTypeCount.has(actionType)) {
+    actionTypeCount.set(actionType, 0)
+  }
+
+  actionTypeCount.set(actionType, actionTypeCount.get(actionType) + 1)
+  console.log(`__ACTION__ (${actionCount}) ${actionType} [${actionTypeCount.get(actionType)}]`)
+
+  return next(action)
+}
