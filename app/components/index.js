6527c8e49c7250f7b5e277fe2ed9ec12aba75751
cglotr <amir.ariffin.920404@gmail.com>
2019-04-11 23:18:17 +0800

    action bar component

--- app/components/index.js
+++ app/components/index.js
@@ -1,3 +1,4 @@
+export { default as ActionBar } from './ActionBar'
 export { default as ActivityIndicator } from './ActivityIndicator'
 export { default as ContactSnippet } from './ContactSnippet'
 export { default as MessageGroupSnippet } from './MessageGroupSnippet'
