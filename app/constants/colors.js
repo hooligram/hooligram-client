0e5781d53571a382df19c9ed6b7f5b05c529c951
cglotr <amir.ariffin.920404@gmail.com>
2019-04-17 23:13:37 +0800

    theme.appcompat.light.noactionbar true white

--- app/constants/colors.js
+++ app/constants/colors.js
@@ -1,9 +1,12 @@
+const SNOW = '#FAFAFA'
+
 export default {
   BLACK: 'black',
   GOLDEN_POPPY: '#FCC200',
   GREY: 'grey',
+  SNOW,
   TEA_GREEN: '#D0F0C0',
   TEAL: '#008080',
-  TRANSLUCENT_WHITE: '#FFFFFFA0',
-  WHITE: 'white'
+  TRANSLUCENT_WHITE: `${SNOW}A0`,
+  WHITE: SNOW
 }
